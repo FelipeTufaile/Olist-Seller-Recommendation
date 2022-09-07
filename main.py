b093f918b43a42fcd9288bb7b5736435cd4c41f3
Tufaile Felipe M <TufaileFelipeM@JohnDeere.com>
2022-09-07 15:30:26 -0300

    fixed bug i function get_seller_customer_distance

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ app = Flask(__name__)
 def get_geolocation(query, api_key):
     return sellers.get_geolocation(query, api_key)
 
-@app.route('/geolocation/<orgn_lat>/<orgn_lng>/<dest_lat>/<dest_lng>/<api_key>')
+@app.route('/geodistance/<orgn_lat>/<orgn_lng>/<dest_lat>/<dest_lng>/<api_key>')
 def get_seller_customer_distance(orgn_lat, orgn_lng, dest_lat, dest_lng, api_key):
     return sellers.get_geolocation(orgn_lat, orgn_lng, dest_lat, dest_lng, api_key)
