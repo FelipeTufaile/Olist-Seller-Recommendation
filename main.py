75a60d4f941d92a7151b81a3715ab3b140ec4fe3
Tufaile Felipe M <TufaileFelipeM@JohnDeere.com>
2022-09-12 00:22:38 -0300

    Updating file main.py

--- main.py
+++ main.py
@@ -12,8 +12,10 @@ def get_geolocation(query, api_key):
 def get_seller_customer_distance(orgn_lat, orgn_lng, dest_lat, dest_lng, api_key):
     return sellers.get_seller_customer_distance(orgn_lat, orgn_lng, dest_lat, dest_lng, api_key)
 
-@app.route('/recommendsellers/<server>/<database>/<username>/<password>/<customer_id>/<product_category>/<payment_installments>/<payment_boleto>/<payment_credit_card>/<payment_voucher>/<payment_debit_card>/<api_key>/<cep>')
+@app.route('/recommendsellers/<server>/<database>/<username>/<password>/<customer_id>/<product_category>/<payment_installments>/<payment_boleto>/<payment_credit_card>/<payment_voucher>/<payment_debit_card>/<api_key>/<priority_delivery>/<priority_review>/<priority_freight>/<cep>')
 def main_function(server, database, username, password, customer_id, product_category, payment_installments, payment_boleto, 
-                  payment_credit_card, payment_voucher, payment_debit_card, api_key, cep):
+                  payment_credit_card, payment_voucher, payment_debit_card, api_key, priority_delivery, priority_review, 
+                  priority_freight, cep):
     return sellers.main_function(server, database, username, password, customer_id, product_category, payment_installments, payment_boleto, 
-                                 payment_credit_card, payment_voucher, payment_debit_card, api_key, cep)
+                                 payment_credit_card, payment_voucher, payment_debit_card, api_key, priority_delivery, priority_review, 
+                                 priority_freight, cep)
